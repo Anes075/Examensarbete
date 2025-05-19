e44d19e65c63f054cd32ea611a01fa95569e519e
Anes075 <166699658+Anes075@users.noreply.github.com>
2025-05-19 19:48:49 +0200

    Added From on cart page

--- frontend/src/components/cartItems/cartItems.jsx
+++ frontend/src/components/cartItems/cartItems.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import "./cartItems.css";
 import { ShopContext } from "../../context/ShopContext";
 
@@ -44,6 +45,7 @@ const CartItems = () => {
         }
         return null;
       })}
+
       <div className="cart-items-down">
         <div className="cart-items-total">
           <h1>Cart Totals</h1>
@@ -63,6 +65,57 @@ const CartItems = () => {
             </div>
           </div>
           <button>Proceed to Checkout</button>
+        </div>
+      </div>
+
+      <div className="delivery-information">
+        <h2>Delivery Information</h2>
+        <div className="delivery-form">
+          <form action="">
+            <div className="delivery-input">
+              <label htmlFor="fname">Name</label>
+              <input type="text" name="fname" id="fname" />
+            </div>
+
+            <div>
+              <label htmlFor="lname">Last Name</label>
+              <input type="text" name="lname" id="lname" />
+            </div>
+
+            <div>
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+
+            <div>
+              <label htmlFor="street">Street</label>
+              <input type="text" name="street" id="street" />
+            </div>
+
+            <div>
+              <label htmlFor="city">City</label>
+              <input type="text" name="city" id="city" />
+            </div>
+
+            <div>
+              <label htmlFor="zipcode">Zipcode</label>
+              <input type="text" name="zipcode" id="zipcode" />
+            </div>
+
+            <div>
+              <label htmlFor="phonenumber">Phone Number</label>
+              <input type="text" name="phonenumber" id="phonenumber" />
+            </div>
+
+            <div>
+              <label htmlFor="paymentmethod">Payment Method</label>
+              <input type="radio" name="stripe" id="stripe" />
+            </div>
+
+            <div>
+              <input type="submit" />
+            </div>
+          </form>
         </div>
       </div>
     </div>
