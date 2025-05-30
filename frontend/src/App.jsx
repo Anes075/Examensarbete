a6e7f60b4ee67bb9cacb734d1057ad17133b0840
Anes075 <166699658+Anes075@users.noreply.github.com>
2025-05-30 14:21:12 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,8 @@ import {
   EmbeddedCheckoutProvider,
   EmbeddedCheckout,
 } from "@stripe/react-stripe-js";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
 import Shop from "./pages/Shop";
 import ShopCategory from "./pages/shopCategory";
 import Product from "./pages/Product";
@@ -62,11 +62,7 @@ const Return = () => {
   if (status === "complete") {
     return (
       <section id="success">
-        <p>
-          We appreciate your business! A confirmation email will be sent to{" "}
-          {customerEmail}. If you have any questions, please email{" "}
-          <a href="mailto:orders@example.com">orders@example.com</a>.
-        </p>
+        <p>Your order has been completed!</p>
       </section>
     );
   }
